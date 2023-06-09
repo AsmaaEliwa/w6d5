8e100e940124083e6970ddf5e34e99a1fc6670ac
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-06-09 17:32:13 -0400

    Started new.html.erb

--- 99Cats/app/controllers/cats_controller.rb
+++ 99Cats/app/controllers/cats_controller.rb
@@ -1,12 +1,31 @@
 class CatsController < ApplicationController
-def index
-   @cats=Cat.all 
-   render  :index
-end
-def show
-    @cat=Cat.find(params[:id]) 
-    render :show
- end
+    def index
+        @cats = Cat.all 
+        render  :index
+    end
+
+    def show
+        @cat = Cat.find(params[:id]) 
+        render :show
+    end
+
+    def new 
+        render :new
+    end 
+
+    def create 
+        @cat = Cat.new(cat_params)
+
+        if @cat.save 
+            redirect_to :show
+        else 
+            render json: @cat.errors.full_messages, status: 422
+        end 
+        
+    end
+
+
+    private 
     def cat_params
         params.require(:cat).permit(:name,:age,:color,:birth_date,:sex)
     end
